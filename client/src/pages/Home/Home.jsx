9e2d24305ceb3dedaff11506a5859f015730f58a
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-02 22:44:10 +0530

    Created Slider in home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Slider from '../../components/Slider/Slider';
 
 const Home = () => {
-    return <div>Home</div>;
+    return <div className="home">
+        <Slider />
+    </div>;
 };
 
 export default Home;
