6f4754eb4cff518ebedc923b9a352676feb66f27
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-02 22:35:52 +0530

    Created Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,59 @@
-
-
+import './Footer.scss';
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <div className="footer">
+            <div className="top">
+                <div className="item">
+                    <h1>Categories</h1>
+                    <span>Women</span>
+                    <span>Men</span>
+                    <span>Shoes</span>
+                    <span>Accessories</span>
+                    <span>New Arrivals</span>
+                </div>
+
+                <div className="item">
+                    <h1>Links</h1>
+                    <span>FAQ</span>
+                    <span>Pages</span>
+                    <span>Stores</span>
+                    <span>Compare</span>
+                    <span>Cookies</span>
+                </div>
+                <div className="item">
+                    <h1>About</h1>
+                    <span>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book.
+                    </span>
+                </div>
+                <div className="item">
+                    <h1>Contact</h1>
+                    <span>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book.
+                    </span>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="left">
+                    <span className="logo">AZ Store</span>
+                    <span className="copyright">
+                        © Copyright 2023. All Rights Reserved
+                    </span>
+                </div>
+                <div className="right">
+                    <img src="/img/payment.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
