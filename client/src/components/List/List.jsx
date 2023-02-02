8e6c68bf9bfeea9b78ff03970248d7ef888b706f
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-02 23:49:24 +0530

    Products page created

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Card from '../Card/Card';
+import './List.scss';
+const List = () => {
+    const data = [
+        {
+            id: 1,
+            title: 'Men',
+            isNew: true,
+            img: 'https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            oldPrice: 20,
+            price: 15,
+        },
+        {
+            id: 2,
+            title: 'Women',
+            isNew: true,
+            img: 'https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            oldPrice: 20,
+            price: 15,
+        },
+        {
+            id: 3,
+            title: 'Boy',
+            img: 'https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            oldPrice: 20,
+            price: 15,
+        },
+        {
+            id: 4,
+            title: 'Girl',
+            img: 'https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            oldPrice: 20,
+            price: 15,
+        },
+    ];
+
+    return (
+        <div className="list">
+            {data.map((item) => (
+                <Card item={item} key={item.id} />
+            ))}
+        </div>
+    );
+};
+
+export default List;
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,7 +1,80 @@
 import React from 'react';
-
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import List from '../../components/List/List.jsx';
+import './Products.scss';
 const Products = () => {
-    return <div>Products</div>;
+    const catId = parseInt(useParams().id);
+    const [maxPrice, setMaxPrice] = useState(1000);
+    const [sort, setSort] = useState(null);
+
+    return (
+        <div className="products">
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Product Categories</h2>
+                    <div className="inputItem">
+                        <input type="checkbox" id="1" value={1} />
+                        <label htmlFor="1">Shoes</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="2" value={2} />
+                        <label htmlFor="2">Skirts</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="3" value={3} />
+                        <label htmlFor="3">Coats</label>
+                    </div>
+                </div>
+
+                <div className="filterItem">
+                    <h2>Filter by price</h2>
+                    <div className="inputItem">
+                        <span>0</span>
+                        <input
+                            type="range"
+                            min={0}
+                            max={1000}
+                            onChange={(e) => setMaxPrice(e.target.value)}
+                        />
+                        <span>{maxPrice}</span>
+                    </div>
+                </div>
+
+                <div className="filterItem">
+                    <h2>Sort by</h2>
+                    <div className="inputItem">
+                        <input
+                            type="radio"
+                            id="asc"
+                            value="asc"
+                            name="price"
+                            onChange={(e) => setSort('acs')}
+                        />
+                        <label htmlfor="asc">Price (Lowest first)</label>
+                    </div>
+                    <div className="inputItem">
+                        <input
+                            type="radio"
+                            id="desc"
+                            value="desc"
+                            name="price"
+                            onChange={(e) => setSort('desc')}
+                        />
+                        <label htmlfor="desc">Price (Highest first)</label>
+                    </div>
+                </div>
+            </div>
+            <div className="right">
+                <img
+                    className="catImg"
+                    src="https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                />
+                <List catId={catId} maxPrice={maxPrice} sort={sort} />
+            </div>
+        </div>
+    );
 };
 
 export default Products;
