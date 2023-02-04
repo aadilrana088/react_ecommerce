2c7e2796bed408ad9895e5302875028edf2ff10f
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-04 23:43:13 +0530

    Remove strapi file and reinstalled strapi project

--- api/config/database.js
+++ api/config/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 8808),
+      port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'store'),
       user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
