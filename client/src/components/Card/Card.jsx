71c0979e5a26945b1ddaae09c603a37cd6eefb14
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-05 17:53:00 +0530

    Polpulated image from server and displayed it

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -8,8 +8,8 @@ const Card = ({ item }) => {
             <div className="card">
                 <div className="image">
                     {item?.attributes.isNew && <span>New Season</span>}
-                    <img src={item.img} alt="" className="mainImg" />
-                    <img src={item.img2} alt="" className="secondImg" />
+                    <img src={process.env.REACT_APP_UPLOAD_URL+item.attributes?.img?.data?.attributes.url} alt="" className="mainImg" />
+                    <img src={process.env.REACT_APP_UPLOAD_URL+item.attributes?.img2?.data?.attributes.url} alt="" className="secondImg" />
                 </div>
                 <h2>{item?.attributes.title}</h2>
                 <div className="prices">
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -9,7 +9,7 @@ const FeaturedProducts = ({ type }) => {
         const fetchData = async () => {
             try {
                 const res = await axios.get(
-                    process.env.REACT_APP_API_URL + '/products',
+                    process.env.REACT_APP_API_URL + '/products?populate=*',
                     {
                         headers: {
                             Authorization:
