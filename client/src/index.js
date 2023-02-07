442cc16d9ecd33c7756cf33d6dcabd8907158fb0
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-08 01:07:54 +0530

    Added persistor but causing issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import store from './redux/store';
 import { Provider } from 'react-redux';
-
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={'loading'} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
