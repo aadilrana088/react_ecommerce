8a65dff350264f8fdd5e35f10495a1555df47828
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-08 01:14:06 +0530

    Redux persistant local storage saved

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
+import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
