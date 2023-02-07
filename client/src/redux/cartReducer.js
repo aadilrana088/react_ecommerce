be2abb98e98a45ec8f3c08be920ffa345f5bf9b4
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-07 23:50:55 +0530

    Added logic for cart and add cart reducer to store

---
+++ client/src/redux/cartReducer.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        products: [],
+    },
+    reducers: {
+        addTocart: (state, action) => {
+            const item = state.products.find((item) => item.id === action.payload.id)
+            if(item) {
+                item.quantity += action.payload.quantity
+            } else {
+                state.products.push(action.payload)
+            }
+        },
+        removeItem: (state, action) => {
+            state.products = state.products.filter((item) => item.id !== action.payload.id)
+        },
+        resetCart: (state) => {
+            state.products = []
+        },
+    },
+});
+// Action creators are generated for each case reducer function
+export const { addTocart, removeItem, resetCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
